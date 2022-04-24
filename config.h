be7f27a2005d9530837e83d29cfdf1b8ec9a82af
Skyblueborb <tomaszborbely0710@gmail.com>
2022-04-24 16:37:11 +0200

    scrollback changes

--- config.h
+++ config.h
@@ -230,8 +230,8 @@ ResourcePref resources[] = {
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ ShiftMask,            Button4, kscrollup,      {.i = 4} },
-	{ ShiftMask,            Button5, kscrolldown,    {.i = 4} },
+	{ XK_NO_MOD,            Button4, kscrollup,      {.i = 4} },
+	{ XK_NO_MOD,            Button5, kscrolldown,    {.i = 4} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
